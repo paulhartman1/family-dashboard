fc0bec705cc2a3fb521faa2128591e165c7836f7
paulhartman1 <paulhartman.bassist@gmail.com>
2025-08-29 12:05:26 -0600

    fixed linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 'use client'
 
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import type { User } from '@supabase/supabase-js'
 
 export default function Home() {
-const [user, setUser] = useState<User | null>(null)
-  const router = useRouter()
-
 
   
   return (
