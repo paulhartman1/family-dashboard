fab1c6dc501cc44fa73c5a693fa15ca120b34032
paulhartman1 <paulhartman.bassist@gmail.com>
2025-08-29 12:13:20 -0600

    restore app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 'use client'
 
 
+import { useEffect, useState } from 'react'
+import { supabase } from '@/utils/supabaseClient'
+import { useRouter } from 'next/navigation'
+import type { User } from '@supabase/supabase-js'
 
 export default function Home() {
+const [user, setUser] = useState<User | null>(null)
+  const router = useRouter()
 
+ useEffect(() => {
+    const getUser = async () => {
+      const { data, error } = await supabase.auth.getUser()
+      if (data?.user) {
+        setUser(data.user)
+        router.push('/dashboard')
+      } else {
+        console.error(error, user)
+        router.push('/login')
+      }
+    }
+    getUser()
+  }, [])
   
   return (
      <p>Redirecting...</p>
